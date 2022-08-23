b5e59877be631f458ee83e6e341cebfb3dc6741e
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-08-23 18:12:35 +0700

    set up dependencies

--- mycv/src/index.js
+++ mycv/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Router, Routes, Route,Navigate } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class HomePage extends Component{
+  state={
+    info: []
+  }
+  render(){
+    return(
+      <div>Hello</div>
+    )
+  }
+}
+export default HomePage;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ScrollToTop />
+    <HomePage style={{ padding: "0px", margin: "0px" }} />
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
